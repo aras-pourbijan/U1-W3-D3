500af990c5f58d1e2f74a96ada72e05f2b73f76a
Aras Pourbijan <arasmidos@gmail.com>
2022-09-14 16:57:46 +0200

    17:00

---
+++ js/script.js
@@ -0,0 +1,78 @@
+function sottrazione(n1 , n2){
+
+    let primoNumero= prompt('inserisci primo numero:')
+    let secondoNumero= prompt('Inserisci secondo numero:')
+   
+    return primoNumero - secondoNumero; 
+
+
+}
+
+let primoNumero;
+let secondoNumero;
+
+let result= sottrazione(primoNumero,secondoNumero)
+
+
+console.log( result)
+
+document.getElementById('diffenza').style
+
+function cambiaColore(colore) {
+   
+    document.getElementById("diffenza").style.backgroundColor = colore
+  }
+
+ if(result>0){
+    cambiaColore(green)
+ }
+ else{
+    cambiaColore(red)
+ }
+
+
+
+
+
+// function sottrazione(n1 , n2){
+
+//     return (n1 - n2);
+// }
+
+// let primoNumero= prompt('inserisci tua eta:')
+// let secondoNumero= prompt('Inserisci la sua eta:')
+
+// console.log( Math.abs(sottrazione(primoNumero,secondoNumero)))
+
+
+
+
+
+
+
+
+
+
+// let tuaEta = prompt("Inserisci tua eta:")
+// let etaDelPartner = prompt("Inserisci eta del tua/tuo partner:")
+
+
+
+
+
+// function sottrazione( n , etaDelPartner)  {
+
+//     lettuaEta - etaDelPartner
+//   }
+
+//  let diff = sottrazione(22)
+  
+//   console.log(diff)
+  
+
+
+  
+
+
+
+
